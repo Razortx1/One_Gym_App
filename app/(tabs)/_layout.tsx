126f8167b3698b9397eacafa0864ffeddb72788d
David Torres <david.torres20022511@gmail.com>
2024-09-14 20:55:03 -0300

    Iconos
    Diferentes tipos de iconos para tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { Redirect, Tabs } from "expo-router";
+import { Momentaneo, Home , Ajustes, Calendario, Pago} from "../../icons/Icons";
 
 export default function IndexLayout() {
     const hola = true
@@ -6,11 +7,11 @@ export default function IndexLayout() {
         return <Redirect href="/initial" />
     }
     else{
-        return <Tabs screenOptions={{headerShown: false}}>
-            <Tabs.Screen name="index" options={{title: "Home"}}/>
-            <Tabs.Screen name="horario" options={{title: "Horario"}}/>
-            <Tabs.Screen name="pagos" options={{title: "Pagar"}}/>
-            <Tabs.Screen name="ajustes" options={{title: "Ajustes"}}/>
+        return <Tabs screenOptions={{headerTitle: ""}}>
+            <Tabs.Screen name="index" options={{title: "", tabBarIcon: Home}}/>
+            <Tabs.Screen name="horario" options={{title: "", tabBarIcon: Calendario}}/>
+            <Tabs.Screen name="pagos" options={{title: "", tabBarIcon: Pago}}/>
+            <Tabs.Screen name="ajustes" options={{title: "", tabBarIcon: Ajustes}}/>
         </Tabs>
     }
 }
---
+++ icons/Icons.tsx
@@ -0,0 +1,23 @@
+import { FontAwesome } from "@expo/vector-icons";
+
+
+export const Momentaneo = () => {
+    return <FontAwesome name="glass" size={24} color="black" />
+}
+
+
+export const Home = () => {
+    return <FontAwesome name="home" size={24} color="black" />
+}
+
+export const Ajustes = () => {
+    return <FontAwesome name="cog" size={24} color="black" />
+}
+
+export const Calendario = () => {
+    return <FontAwesome name="calendar" size={24} color="black" />
+}
+
+export const Pago = () => {
+    return <FontAwesome name="dollar" size={24} color="black" />
+}
